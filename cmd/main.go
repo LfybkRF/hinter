e8fb312697fc9ad23e5294e6972aa6b9ba6f273a
LfybkRF <daniil_zohno@mail.ru>
2024-01-10 10:31:25 +0300

    add main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,69 @@
 package main
 
-
 import (
-	stan "github.com/nats-io/stan.go"
+	"l0_ms/internal/app/server"
+	"l0_ms/internal/config"
+	"l0_ms/internal/dao"
+	"l0_ms/internal/service"
 	"log"
+	"os"
 )
 
 
 func main() {
-	log.Printf("Starting Gateway\n")
-	s, err := stan.Connect("demo", "demo")
-	if err!= nil {
-        log.Fatalf("Error: %s\n", err)
-    }
-	defer s.Close()
-	log.Printf("Gateway started\n")
-	s.Subscribe("demo.>", func(msg *stan.Msg) {
-		log.Printf("Received message: %s\n", msg.Data)
-    })
-	
+
+	configPath := os.Getenv("CONFIG_PATH")
+
+	configure, err := config.NewConfig(configPath)
+	if err != nil {
+		log.Fatalf("failed to Config %v", err)
+	}
+
+	// Открытие соединения с postgres
+	dbClient, err := config.DatabaseConnect(configure.Database)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	log.Printf("connecting to the database: success")
+
+	defer func() {
+		dbInstance, _ := dbClient.DB()
+		if err = dbInstance.Close(); err != nil {
+			log.Fatalf("failed to close database: %v", err)
+		}
+	}()
+
+	// Клиент для реализации бизнес-логики
+	client := dao.NewOrderClient(dbClient)
+	err = client.Start()
+	if err != nil {
+		log.Fatalf("failed Start: %v", err)
+	}
+
+	natsClient, err := config.NatsStreamingConnect(configure.NatsStreaming)
+	if err != nil {
+		log.Fatalf("failed to connect to nats-streaming: %v", err)
+	}
+
+	log.Printf("connecting to the nats-streaming: success")
+
+	defer func() {
+		if err = natsClient.Close(); err != nil {
+			log.Fatalf("failed to close nats-streaming: %v", err)
+		}
+	}()
+
+	// Клиент для nats-streaming
+	natsStreaming := service.NewSubscriber(configure.NatsStreaming.Channel, natsClient, client)
+
+	natsStreaming.NatsSteamingSubscribe()
+
+	// Запуск http сервера
+	httpRouter := server.NewHttpRouter(configure.Server, client)
+	log.Printf("start http-server")
+	err = httpRouter.Start()
+	if err != nil {
+		log.Fatalf("failed to listen the port: %d %v", configure.Server.Port, err)
+	}
 }
